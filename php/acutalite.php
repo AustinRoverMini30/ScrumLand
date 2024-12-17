65865b452895f43b8a1aea629f16492074b4456e
axelgallard <axelgallard72@gmail.com>
2024-12-17 10:43:51 +0100

    Réglage des problèmes avec JS

--- php/acutalite.php
+++ php/acutalite.php
@@ -10,8 +10,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/Assets/CSS/actualite.css">
-    <script src="/JS/actualite.js" defer></script>
+    <link rel="stylesheet" href="Assets/CSS/actualite.css">
+    <script src="Assets/JS/actualite.js" defer></script>
     <title>Actualités</title>
 </head>
 <body>
